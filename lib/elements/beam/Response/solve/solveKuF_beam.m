a7495afcff57fe26f7baa8c314dd2e305cd3c13e
jchapuis <jchapuis@mavt-edac-n003w>
2024-06-12 10:45:47 +0200

    Removed a check that caused additional overhead in the beam solver.

--- lib/elements/beam/Response/solve/solveKuF_beam.m
+++ lib/elements/beam/Response/solve/solveKuF_beam.m
@@ -7,11 +7,4 @@ u(idfDOF,:) = mldivide( Km(idfDOF,idfDOF), F(idfDOF)-Km(idfDOF,idpDOF)*u(idpDOF,
 % Solve for reactions:
 F(idpDOF,:)=Km(idpDOF,idfDOF)*u(idfDOF,:)+Km(idpDOF,idpDOF)*u(idpDOF,:);
 
-% check if global stiffness matrix is badly conditioned
-Kmcond = condest(Km(idfDOF,idfDOF));
-if Kmcond >= 1e12
-    fprintf(2,'Warning: Matrix is close to singular or badly scaled. Results may be inaccurate. CONDEST =  %.4e.\n', Kmcond);
-end
-
-
 end
